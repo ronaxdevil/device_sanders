94baefc449e0f40001c5302107b51e0fe75ae115
ronaxdevil <pratabidya.007@gmail.com>
2019-06-14 20:58:57 +0000

    sanders : Bconfig: Enable D2W node
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,7 +147,8 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_NO_CHARGER_LED := true
 
 # DT2W
-#TARGET_TAP_TO_WAKE_NODE := "/sys/android_touch/doubletap2wake"
+BOARD_USES_CYANOGEN_HARDWARE := true
+TARGET_TAP_TO_WAKE_NODE := "/sys/android_touch/doubletap2wake"
 
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
