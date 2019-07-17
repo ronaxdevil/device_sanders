88a9dcf8a47f6ef3122cebed7d20aafbf0ca5b03
ronaxdevil <pratabidya.007@gmail.com>
2019-07-17 20:48:42 +0530

    sanders: Add dtv init file to fix dtv
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com>

--- device.mk
+++ device.mk
@@ -360,7 +360,9 @@ PRODUCT_PACKAGES += \
     init.qcom.sensors.sh \
     init.sanders.rc \
     init.spectrum.rc \
-    init.spectrum.sh
+    init.spectrum.sh \
+    init.dtv.rc \
+    init.dtv.sh
 
 PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/rootdir/etc/ueventd.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -107,3 +107,19 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.spectrum.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.dtv.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.dtv.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.dtv.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.dtv.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
