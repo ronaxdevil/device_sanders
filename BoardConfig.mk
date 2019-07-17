91257f36f66550afa5f3a86c2862adf585a87ced
ronaxdevil <pratabidya.007@gmail.com>
2019-07-17 14:11:19 +0530

    sanders :fix clangpath
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,7 +72,7 @@ TARGET_KERNEL_CONFIG := sanders_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8953
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-TARGET_KERNEL_CLANG_PATH := $(PWD)/prebuilts/clang/host/$(HOST_OS)-x86/clang-5696680/bin
+TARGET_KERNEL_CLANG_PATH := $(PWD)/prebuilts/clang/host/$(HOST_OS)-x86/clang-r353983e/bin
 TARGET_KERNEL_CLANG_VERSION := 9.0.5
 TARGET_USE_SDCLANG := true
 
