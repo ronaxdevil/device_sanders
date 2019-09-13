fdda486147eb6d7e4986c3a58cc4c66ad7b5f865
sai4041412 <sai404142@gmail.com>
2019-09-13 08:27:37 +0000

    Sanders : Android Q move to dirty sepolicy[TEMP]
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -244,7 +244,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # SELinux
 #include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 #Soong
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
