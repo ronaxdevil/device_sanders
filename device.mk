fa7633587c2e3af11b1dc8c2d2c85408c7dcb5a8
kubersharma001 <kubersharma001@gmail.com>
2019-09-17 15:06:26 +0530

    sanders: prebuilt: [DNM][TEMP] Use OpenCamera for now
     * This is just a temporary change because Camera2 is very glitchly and randomly crashes, so until that is fixed lets use this.
    
     * More Info:
     https://sourceforge.net/p/opencamera/code/
     https://opencamera.sourceforge.io/
     https://sourceforge.net/projects/opencamera/files/
    
    Change-Id: Ibd4a78db4457b432e166f114f0bb8d5e56378b9f
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com>

--- device.mk
+++ device.mk
@@ -106,8 +106,6 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     libbson \
-    Snap2 \
-    Camera2 \
     libgui_vendor \
     camera_msm8953_shim \
     camera.device@1.0-impl \
@@ -115,7 +113,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    vendor.qti.hardware.camera.device@1.0_vendor \
+    opencamera
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera/imx219_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/imx219_chromatix.xml \
