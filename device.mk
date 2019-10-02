1f55cfa5590dbb528e772864e1f9250958dac99c
sai4041412 <sai404142@gmail.com>
2019-10-02 14:00:00 +0100

    let's build android.hardware.camera.provider@2.4-service along with camera2
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com>

--- device.mk
+++ device.mk
@@ -106,11 +106,13 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     libbson \
-    SnapdragonCamera2 \
+    Snap2 \
+    Camera2 \
     libgui_vendor \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
