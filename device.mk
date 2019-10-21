741be4a76589a0c54d149e5e7d8b6103d22202ca
ronaxdevil <pratabidya.007@gmail.com>
2019-10-21 17:59:41 +0530

    Revert "sanders: import CAF binderized power hal"
    This reverts commit f82c771dda6b7c7fcf7f79d7bf52a8ad54396a7a.
    
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com>

--- device.mk
+++ device.mk
@@ -327,7 +327,7 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
-    android.hardware.power@1.2-service \
+    android.hardware.power@1.0-service \
     power.qcom
 
 # Powerhint configuration file
