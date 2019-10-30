41398f232f81ad9fbd8bc6a68eee915a7e2a7797
ronaxdevil <pratabidya.007@gmail.com>
2019-10-30 17:07:52 +0530

    Sanders : init.spectrum.rc/sh: LOCAL_MODULE_TAGS := eng debug are obsolete
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -94,7 +94,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.spectrum.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.spectrum.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -102,7 +102,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.spectrum.sh
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.spectrum.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
