54066e1a591528abc9701f1eacf71e92e4fef50f
ronaxdevil <pratabidya.007@gmail.com>
2019-11-29 09:55:54 +0530

    sanders: update power hal

--- device.mk
+++ device.mk
@@ -326,7 +326,7 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service \
+    android.hardware.power@1.2-service \
     power.qcom
 
 # Powerhint configuration file
