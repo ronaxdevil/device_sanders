d989e5542c42f013107d709c958bbacec48b0733
intervigilium <intervigil@gmail.com>
2020-01-23 20:19:07 +0530

    sanders: Add AOSP RCS packages
    Change-Id: Ic16d3b92820d246db73ad70ccaa0f4bc7f50e515
    Signed-off-by: Melvin <kartik3163@gmail.com>

--- device.mk
+++ device.mk
@@ -237,7 +237,9 @@ PRODUCT_BOOT_JARS += \
 
 # IMS
 PRODUCT_PACKAGES += \
-    com.android.ims.rcsmanager
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService
 
 # IRSC
 PRODUCT_COPY_FILES += \
