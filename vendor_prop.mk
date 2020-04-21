557b0a518c60a9f14281319b75cf2ce9b94576f6
DennySPB <dennyspb@gmail.com>
2020-04-21 14:07:12 +0200

    sanders: props: use vulkan as default renderer
    some users reported that it's increase SOT and makes SystemUI smoother
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I8ef5f79d8a05ab9430681fc4f6e5782431936408
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -148,7 +148,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.enable.sglscale=1 \
     vendor.display.enable_default_color_mode=1 \
     vendor.gralloc.enable_fb_ubwc=1 \
-    vendor.display.disable_skip_validate=1
+    vendor.display.disable_skip_validate=1 \
+    debug.hwui.renderer=skiavk
 
 # Enable QC2 bufferqueue block-pool
 PRODUCT_PROPERTY_OVERRIDES += \
