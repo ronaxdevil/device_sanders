05fe28984df0244ea6c535e109bf4dbcff6e376e
Kuber Sharma <kubersharma001@gmail.com>
2020-04-28 09:13:49 +0530

    sanders: checkfeatures: Add missing "/"
    Signed-off-by: ashwatthama <sai404142@gmail.com>
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com>

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -22,8 +22,8 @@ if ! [ "$sku" = "XT1802" ]; then
     rm /vendor/lib64/libdtvtuner.so
     rm /vendor/lib/libdtvhal.so
     rm /vendor/lib64/libdtvhal.so
-    rm -r /system/system/priv-app/DTVPlayer
-    rm -r /system/system/priv-app/DTVService
+    rm -r /system/system/app/DTVPlayer
+    rm -r /system/system/app/DTVService
 fi
 
 if [ "$sku" = "XT1804" ]; then
